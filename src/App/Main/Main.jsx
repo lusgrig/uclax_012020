82fc5a6f04138735c993d55ecb8589d193063e72
Grigoryan <206584873@lgrigoryan-mac-8.local>
2020-01-22 21:38:03 -0800

    final work

--- src/App/Main/Main.jsx
+++ src/App/Main/Main.jsx
@@ -3,7 +3,16 @@ import './Main.scss';
 
 const Main = () => {
     return (
-        <main className='Main'>Component Main</main>
+        <main className='Main'>
+            <div className="columns">
+                <div className="col col1">Col 1</div>
+                <div className="col col2">Col 2</div>
+                <div className="col col3">Col 3</div>
+                <div className="col col4">Col 4</div>
+
+            </div>
+        
+        </main>
     )
 };
 
--- src/App/Nav/Nav.jsx
+++ src/App/Nav/Nav.jsx
@@ -1,16 +1,41 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useState } from 'react';
 import './Nav.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 const Nav = () => {
-    return (
+
+    const [showMenu, updateShowMenu] = useState(true);
+
+    const handleHamburgerClick = () => {
+        console.log('you hit the hamburger');
+        let newShowMenu;
+        if(showMenu === true){
+            newShowMenu = false;
+        } else{
+            newShowMenu = true;
+        }
+        updateShowMenu(newShowMenu);
+
+    }
+
+    return (    
         <nav>
-            <div className="hamburger">Hamburger</div>
-            <div className="links">
-                <a href="#">Welcome</a>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
-                
+            <div className="hamburger" onClick={handleHamburgerClick}>
+                <FontAwesomeIcon icon = { faBars }/>
+            
             </div>
+            {
+                showMenu &&
+                <div className="links">
+                    <a href="#">Welcome</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                    
+                </div>
+            }  
+            
         </nav>
     );    
 }
